9cd23f9a17c864acfcd4bc09cad05f36b287399e
WarrenMfg <warrenmfg@icloud.com>
2020-05-19 20:17:07 -0400

    added spinner

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/Spinner';
 
 
 class Dashboard extends React.Component {
@@ -10,20 +11,44 @@ class Dashboard extends React.Component {
   }
 
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />
+    } else {
+      dashboardContent = <h1>Hello</h1>
+    }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 Dashboard.propTypes = {
-
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
 
 const mapDispatchToProps = {
   getCurrentProfile
 };
 
-export default connect(null, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
