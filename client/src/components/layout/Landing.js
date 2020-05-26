fc1e7fe68c16a2b3c8b439429cad7d00cd497386
WarrenMfg <warrenmfg@icloud.com>
2020-05-26 16:13:36 -0400

    changed landing page description line

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -20,7 +20,7 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">devConnector</h1>
-                <p className="lead"> Create a developer profile, share tips, and get help from other developers</p>
+                <p className="lead"> Create a developer profile, share recent repos, and chat with other developers</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                 <Link to="/login" className="btn btn-lg btn-light">Login</Link>
