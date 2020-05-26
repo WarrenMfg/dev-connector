6ab89f99d9db0a68ae275b27fc225c15f80877cc
WarrenMfg <warrenmfg@icloud.com>
2020-05-26 09:02:32 -0400

    reverted to original entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
+  entry: path.resolve(__dirname, 'client/src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'client', 'public')
+    path: path.resolve(__dirname, 'client/public')
   },
   module: {
     rules: [
