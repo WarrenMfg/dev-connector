a2dd8cec48f13d67854e9cb036ee972124733bc4
WarrenMfg <warrenmfg@icloud.com>
2020-05-30 20:19:01 -0400

    added conditional for recruiter account; updated css

--- backend/server/index.js
+++ backend/server/index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
           // for loginRequired middleware
           req.user = decode;
           // to protect recruiter login account
-          // if ( req.user.userName === 'kent-warren' && (req.method === 'POST' || req.method === 'DELETE') ) return res.end();
+          if ( req.user.userName === 'kent-warren' && (req.method === 'POST' || req.method === 'DELETE') ) return res.end();
         }
         next();
       });
